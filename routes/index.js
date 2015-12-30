ed4f1179552036016ccd62ffa99da126565aad02
fangxuecong <fangxuecong@meizu.com>
2015-12-30 10:37:27 +0800

    view signin page

--- routes/index.js
+++ routes/index.js
@@ -12,13 +12,19 @@ router.get('/', function(req, res, next) {
 
   res.render('index', sendToIndexEjs);
 });
-
+/* GET signup page. */
 router.get('/signup',function(req,res,next){
   var sendToSignUpEjs={
     title:'注册 - Node.js'
   };
   res.render('signup',sendToSignUpEjs);
 });
-
+/* GET signin page. */
+router.get('/signin',function(req,res,next){
+  var sendToSignInEjs={
+    title:'登入 - Node.js'
+  };
+  res.render('signin',sendToSignInEjs);
+});
 
 module.exports = router;
